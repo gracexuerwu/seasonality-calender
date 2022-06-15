1f29a6381b8e676b6314b7430f9d047efa1de32e
Grace. W <grace.xuer.wu@gmail.com>
2022-06-15 15:29:09 +0200

    Current Month

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -9,12 +9,21 @@ export default function Introduction() {
         <div className="Introduction">
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-3">
               <img src={placeholderImage} alt="" />
             </div>
-            <div className="col">
+            <div className="col-9">
               <h1>What grows here in Switzerland?</h1>
               <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h2>
+            </div>
+          </div>
+          <div className="row introduction-rowTwo">
+            <div className="col-9">
+              <h1>Why buy seasonal and regional food?</h1>
+              <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h2>
+            </div>
+            <div className="col-3">
+              <img src={placeholderImage} alt="" />
             </div>
           </div>
         </div>
