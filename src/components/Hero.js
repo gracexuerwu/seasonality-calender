9e93306f881d928f47e2386e82c3e90a359cf2dc
Grace. W <grace.xuer.wu@gmail.com>
2022-06-15 07:51:19 +0200

    Initialize react app

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Hero.css";
+
+export default function Hero() {
+    return (
+        <div className="Hero">
+            <h1>Swiss seasonality calender</h1>
+            </div>
+    );
+}
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Home.css";
+
+//Component import
+import Hero from "./Hero";
+
+export default function Home() {
+    return (
+        <div>
+            <Hero />
+        </div>
+    );
+}
